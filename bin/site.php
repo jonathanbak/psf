6398bc7c2abe96e00fc557ad16e6a8bbdb3103d8
sangwonbak <jonathanbak@gmail.com>
2018-05-15 15:59:45 +0900

    install 시 composer autoloader 위치 수정

--- bin/site.php
+++ bin/site.php
@@ -1,7 +1,6 @@
 <?php
-$baseDir = dirname(dirname(__FILE__));
-$composerAutoloader = require_once($baseDir . '/vendor/autoload.php');
 define('ROOT_DIR', getcwd());
+$composerAutoloader = require_once(ROOT_DIR . '/vendor/autoload.php');
 use PSF\Application as App;
 use PSF\Installer;
 
